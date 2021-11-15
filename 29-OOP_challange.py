66ad9351390ecca2f83e247f76640aeea7e85e73
Marcin <91530764+Mgalazyn@users.noreply.github.com>
2021-11-15 16:24:06 +0100

    Update 29-OOP_challange.py

--- 29-OOP_challange.py
+++ 29-OOP_challange.py
@@ -6,18 +6,22 @@ class Account:
     
     def deposit(self,value):
         print('Deposite Accepted')
-        return self.balance + value
+        self.balance = self.balance + value
+        return self.balance
 
     def withdraw(self,value1):
-        print('Withdraw Succesful')
-        return self.balance - value1
-    
+        if self.balance >= value1:
+            self.balance =  self.balance - value1
+            print('Withdraw Succesful')
+        else:
+            print('Sorry, not enough funds!')
+        return self.balance
     def __str__(self):
         return f'Account owner: {self.owner}\nAccount Balance: {self.balance} '
 
 ac1 = Account('Marcin', 1000)
 
 print(ac1.deposit(100))
-print(ac1.withdraw(10))
+print(ac1.withdraw(100))
 
 print(ac1)
